11faa4ddee321c60a9e4466db5dbfe944aa451fa
DOUGLAS FELIPE MARIANO <douglas.mariano01>
2024-02-15 20:41:15 -0300

    Aula 15-02-24

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("Hello World!")
+
+let nome="Doga";
+
+console.log(nome);
+
+let idade = 25;
+let altura = 1.75;
+
+console.log('nome:' + nome + " - idade:" +idade+ ' - altura:' +altura);
+
+let trabalha=true;
+
+console.log(trabalha);
+
+console.log("trabalha: " + (trabalha == true ? "Sim" : "Não" ));
+
+const sangue = "A+";
+
+let endereco ={
+    "rua" : "Sergio Bevilaqua",
+    "num" : "80",
+    "Bairro" : "Ant. Per. Gaspar",
+    "UF" : "SP",
+    "mora" : true
+}
+
+console.log(endereco);
+
+console.log(typeof (endereco));
+
+let fruta = ["pequi","tumate","bóbora"];
+
+console.log(fruta);
+
+console.log(fruta [0]);
+
+let velha= [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+console.log(velha[2][2]);
