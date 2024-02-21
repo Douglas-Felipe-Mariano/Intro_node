8d22e5f317de7c2242f3f54498fb7a16dfb270f6
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-02-20 22:28:44 -0300

    Intro_Node-20/02

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-console.log("Hello World!")
+console.log("Hello World!") 
 
-let nome="Doga";
+let nome="Doga"; //let é uma das formas de declaração de variavel, sendo ela de varaivel "local" - Tipo de variavel: String
 
 console.log(nome);
 
-let idade = 25;
+let idade = 25; // Tipo de variavel: Number
 let altura = 1.75;
 
 console.log('nome:' + nome + " - idade:" +idade+ ' - altura:' +altura);
 
-let trabalha=true;
+let trabalha=true; // Tipo de Variavel: Bolean
 
-console.log(trabalha);
+console.log(trabalha); // Exibe a Varivael trabalha
 
-console.log("trabalha: " + (trabalha == true ? "Sim" : "Não" ));
+console.log("trabalha: " + (trabalha == true ? "Sim" : "Não" )); // Exibe a variavel trabalha por meio de um operador ternario, ou um "if simplificado"
 
-const sangue = "A+";
+const sangue = "A+"; // Variavel constante, apenas obtem um valor imutavel, não tem tipo
 
-let endereco ={
+let endereco ={ //Variavel em tipo de objeto
     "rua" : "Sergio Bevilaqua",
     "num" : "80",
     "Bairro" : "Ant. Per. Gaspar",
@@ -29,16 +29,19 @@ console.log(endereco);
 
 console.log(typeof (endereco));
 
-let fruta = ["pequi","tumate","bóbora"];
+let fruta = ["pequi","tumate","bóbora"]; // Array de uma dimensão
 
 console.log(fruta);
 
-console.log(fruta [0]);
+console.log(fruta [0]); // Mostra o item da posição 0 do array fruta
 
-let velha= [
+let velha= [ // Array de duas dimensões
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8]
 ];
 
 console.log(velha[2][2]);
+console.log(typeof(velha)); // Demonstra o tipo de varivel "velha"
+
+console.log((Array.isArray(velha)) ? "Array" : "Não é Array"); // Exibe a variavel trabalha por meio de um operador ternario, ou um "if simplificado"
