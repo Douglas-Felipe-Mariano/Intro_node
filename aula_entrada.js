60c8013937e72e916cdd6cb5ed814ed33643eb74
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-02-20 22:49:57 -0300

    Aula 20-02-24

---
+++ aula_entrada.js
@@ -0,0 +1,9 @@
+let entrada = require("prompt-sync")();
+
+let nome;
+
+nome = entrada("Digite sue nome: ");
+
+console.log("O nome digitado foi: " + nome);
+
+entrada ("Pressione enter para encerar!");
