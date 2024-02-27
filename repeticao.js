ceb159b7ba1986330c781e0aff5894db6565afbf
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-02-27 20:39:48 -0300

    Aula Lógica 27-02

--- repeticao.js
+++ repeticao.js
@@ -11,7 +11,7 @@ let entrada = require("prompt-sync")();
 // }
 
 
-let num = parseInt(entrada("Digite um numero para visualizar a tabuada! \n"));
+// let num = parseInt(entrada("Digite um numero para visualizar a tabuada! \n"));
 // let cont = 1;
 
 // while (cont <= 10){
@@ -25,9 +25,64 @@ let num = parseInt(entrada("Digite um numero para visualizar a tabuada! \n"));
 //     console.log(num+"x"+cont+"="+calc);
 // }
 
-let cont=1;
-do{
-    let calc = num * cont;
-    console.log(num+"x"+cont+"="+calc);
-    cont = cont + 1;
-} while(cont <= 10)
+// let cont=1;
+// do{
+//     let calc = num * cont;
+//     console.log(num+"x"+cont+"="+calc);
+//     cont = cont + 1;
+// } while(cont <= 10)
+// const senha = "123456";
+
+// do {
+//     if (cont == 3){
+//         break;
+//     }
+//         cont++;
+//       var senhaDig = entrada("Digite a senha para acessar: ");
+// }while (senha !== senhaDig);
+
+// if (cont >= 3){
+//     console.log("'Acesso Não Autorizado'");
+// }else {
+//     console.log("Acesso Autorizado");
+// }
+
+
+// for (cont=1; cont <= 3; cont ++){
+//     var senhaDig= entrada("Digite a senha para acessar: ");
+// if(cont <=3 ){    
+//     if (senha == senhaDig ){
+//         console.log("Acesso Autorizado");
+//         break;
+//     }
+//     if (cont ==3 ) {
+//         console.log("acesso negado!");
+//     }
+//     }
+// }
+
+let senha ="1234";
+let snCorreta = false;
+
+for (let index =1; index <= 3; index++){
+    const snDg = entrada("Digite a Senha: ");
+    if (senha === snDg){
+        snCorreta = true;
+        break
+    }
+}
+
+console.log(snCorreta ? "Acesso Autorizado" : "Acesso negado");
+
+
+
+    
+
+
+
+// let sn = '123456';
+// let snDg = entrada ("Digite a senha para acessar o sistema: ");
+
+// while(sn ==! snDg){
+//     snDg = entrada("Digite a senha para acessar o sistema: ");
+// }
